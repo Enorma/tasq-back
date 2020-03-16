c369441e64e3e0a2674214cd2fe647257e7ba547
Enorma <enorma29@gmail.com>
2020-03-15 23:08:45 -0600

    Batch 8

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const todoRoutes = express.Router();
-const PORT = 4000;
 
 let Todo = require('./todo.model');
+
+const app = express();
+const todoRoutes = express.Router();
+const PORT = 4000;
 
 app.use(cors());
 app.use(bodyParser.json());
